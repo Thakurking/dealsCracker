bcf64131406bddcafc16c37369e4fc0a97bc54e1
Thakurking <riteshkumar3310@gmail.com>
2021-04-09 11:49:47 +0530

    setup jwt middleware

--- backend/middleware/jwt.Middleware.js
+++ backend/middleware/jwt.Middleware.js
@@ -1,5 +1,30 @@
-const jwt = require("jsonwebtoken");
+const token = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
-    
+  try {
+    const { jwt } = req.headers;
+    console.log(jwt);
+    if (!jwt) {
+      return res.json({ message: "Access Failed", isSuccess: false });
+    }
+    const auth = jwt.replace("Bearer ", "");
+    token.verify(auth, process.env.JWT_SECRET, async (err, payload) => {
+      if (err) {
+        return res.json({
+          message: "Token Authorization Failed",
+          isSuccess: false,
+        });
+      }
+      if (payload.client && payload.user) {
+        req.user = payload.user;
+        req.client = true;
+        next();
+      } else {
+        return res.json({ message: "User Not Authorized", isSuccess: false });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({ message: "Internal Server Error", isSuccess: false });
+  }
 };
